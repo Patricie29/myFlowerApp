9d634e9b769b1f2acde873058eec025698eb0d56
Patricie <puclici@Patricies-MacBook-Air.local>
2023-03-22 13:50:55 +1300

    final edit

--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -4,15 +4,14 @@ import logo from '../../images/logo.svg'
 import Card from '../UI/Card'
 
 
-const NavBar = ({setShowCart}) => {
+const NavBar = ({ setShowCart }) => {
     return <div className={styles['main-page']}>
         <div className={styles['main-image']}>
             <header className={styles.header}>
-                {/* <img src={"https://kytkajakobrno.cz/wp-content/uploads/2020/09/logo2.svg"} alt="" className={styles.logo}/>  */}
-                <img src={logo} alt="" className={styles.logo}/> 
+                <img src={logo} alt="" className={styles.logo} />
 
-                <NavbarCartButton setShowCart={setShowCart}/>
-            </header> 
+                <NavbarCartButton setShowCart={setShowCart} />
+            </header>
         </div>
         <Card className={styles.mainCard}>
             <h4>Welcome to Kytka jako Brno!</h4>
